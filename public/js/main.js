12ea21a82c8e21a81cef369a44a481994b442e5e
仲田 洋子 <nakadahiroko@nakata-no-MacBook-Pro.local>
2014-11-28 22:04:34 +0900

    jsmousukoshi

--- public/js/main.js
+++ public/js/main.js
@@ -116,23 +116,17 @@ var name = Parse.Object.extend("age");
  var User = Parse.Object.extend("User");
  var user = new Parse.User();
 user.set("name", name);
-var user = new Parse.User();
-user.set("email", name);
-var user = new Parse.User();
-user.set("password", name);
-var user = new Parse.User();
-user.set("area", name);
-var user = new Parse.User();
-user.set("age", name);
+
+
 
 
 
 user.signUp(null, {
   success: function(user) {
-    ご登録ありがとうございます。// Hooray! Let them use the app now.
+   var messageField = $("#message");
+messageField.text("ご登録ありがとうございます。");
   },
   error: function(user, error) {
-    もう一度試して下さい。// Show the error message somewhere and let the user try again.
     alert("Error: " + error.code + " " + error.message);
   }
 
@@ -171,3 +165,82 @@ $("loginBtn").click(function () {
 });
 
 
+
+
+
+
+
+
+
+jQuery.event.add(window,"load",function(){
+ 
+    //リサイズイベント
+    $(window).resize(function(){
+        //コンテンツの高さ設定
+        hoge.contentHeight();
+    });
+     
+    // 以下、パララックス機能の記述
+    hoge.parallax();
+ 
+});
+ 
+var hoge = [];
+ 
+// ウィンドウサイズによってコンテンツの高さを変える
+hoge.contentHeight = function(){
+    var winHeight = $(window).height();
+}
+ 
+// パララックス機能
+hoge.parallax = function(){
+ 
+    // アニメーション要素を包括している要素を指定
+    var containerDiv = $('#container');
+    // アニメーションの変数を宣言
+    var container = ScrollTween.container(containerDiv);
+ 
+        // 変数に .add でアニメーションを追加していく
+        container.add("#cnt1", function(tween) {
+            tween
+                        // スクロール値「100」時点で、上にはけた状態で中央（水平）方向に存在している
+                        .to(100, tween.styles().topOut().center())
+                        // スクロール値「200」から「300」間で、中央（垂直）方向に 200 px の位置でストップする
+                        // スクロール値「200」から「300」間の処理（変化）ではないことに注意！！
+                        .range(200, 300, tween.styles().middle(200))
+                        // スクロール値「400」時点で、右方向にはける
+                        .to(400, tween.styles().rightOut());
+        });
+        container.add("#cnt2", function(tween) {
+            tween
+                        // スクロール値「200」時点で、下にはけた状態で中央（水平）方向に存在している
+                        .to(200, tween.styles().bottomOut().center())
+                        // スクロール値「300」から「400」間で、中央（垂直）の位置でストップする
+                        .range(300, 400, tween.styles().middle())
+                        // スクロール値「500」時点で、左方向にはける
+                        .to(500, tween.styles().leftOut());
+        });
+        container.add("#cnt3", function(tween) {
+            tween
+                        .to(400, tween.styles().center().middle())
+                        // {progress:～}で直接 css をあやつれる
+                        .to(400, 0 , { progress: function (tween) { $("#cnt3").css("opacity", 0) } })
+                        // opacity 部分は、0 から tween 分がプラスされるイメージ
+                        .to(600, 0 , { progress: function (tween) { $("#cnt3").css("opacity", tween) } })
+                        .to(800, 0 , { progress: function (tween) { $("#cnt3").css("opacity", 1) } })
+                        // opacity 部分は、1 から tween 分がマイナスされるイメージ
+                        .to(1000, 0 , { progress: function (tween) { $("#cnt3").css("opacity", 1 - tween) } });
+        });
+ 
+        // アンカーリンクをつける
+        $('#nav .list:nth-child(2) a').stop().on('click', function(){
+            // 引数にはスクロール値が入ります
+            container.scrollTo(1000);
+            return false;
+        });
+ 
+        // ちょっと処理ずらして、アニメーションを再生する
+        setTimeout(function(){
+            container.play();
+        },300);
+}
