6f8077c8de61b439a9740f2b3646709bd09d6db7
仲田 洋子 <nakadahiroko@nakata-no-MacBook-Pro.local>
2015-04-12 22:12:25 +0900

    fix map

--- public/js/index1.js
+++ public/js/index1.js
@@ -1,64 +1,158 @@
-$(function () {
-    var currentUser = Parse.User.current();    // Parse ユーザを取得
+$(function() {
+    var currentUser = Parse.User.current(); // Parse ユーザを取得
 
-    if (currentUser) {    // ログインしている場合
+    if (currentUser) { // ログインしている場合
         console.log("Already logged in. Redirecting to index2.");
-        location.replace("index2.html");    // index2.html へ移動
+        location.replace("index2.html"); // index2.html へ移動
     } else {
         console.log("Not logged in yet.");
     }
-         var logo = $('h1.logo');
-          $(window).scroll(function () {
-            if($(window).scrollTop() > 300) {
-              logo.addClass('logo-scroll');
-            } else {
-              logo.removeClass('logo-scroll');
-            }
-          });
+    var logo = $('h1.logo');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            logo.addClass('logo-scroll');
+        } else {
+            logo.removeClass('logo-scroll');
+        }
+    });
 });
 
-      window.onload = function(){
-        var lat = 26.240362;
-        var lng = 127.711596;
-        var lat2 = 26.240355;
-        var lng2 = 127.711580;
-        var map = new GMaps({
-            div: "#map",
-            lat: lat,
-            lng: lng,
-            zoom: 10
-        });
-
-        map.addMarker({
-            lat: lat,
-            lng: lng,
-            title: "ROLE MODEL",
-            icon: "/img/logo0312.png",
-            infoWindow: {
-                content: "<h2>タイトル</h2><p><br/>説明文が入ります。</p>"
-            }
-        });
-         map.addMarker({
-            lat: lat2,
-            lng: lng2,
-            title: "ROLE MODEL",
-            icon: "/img/logo.png",
-            infoWindow: {
-                content: "<h2>タイトル</h2><p><br/>説明文が入ります。</p>"
-            }
-        });
+window.onload = function() {
+    var lat = 26.240362;
+    var lng = 127.711596;
+    var lat2 = 26.240355;
+    var lng2 = 127.711580;
+    var map = new GMaps({
+        div: "#map",
+        lat: lat,
+        lng: lng,
+        zoom: 10
+    });
 
 
-        var styles = [
-           {[{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"},{"visibility":"on"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"saturation":"17"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#66c9f0"},{"visibility":"on"}]}]
-        ];
+    map.addMarker({
+        lat: lat,
+        lng: lng,
+        title: "ROLE MODEL",
+        icon: "/img/logoo8.png",
+        infoWindow: {
+            content: "<h2>タイトル</h2><p><br/>説明文が入ります。</p>"
+        }
+    });
 
-        map.addStyle({
-            styledMapName:"Styled Map",
-            styles: styles,
-            mapTypeId: "map_style"
-        });
+    map.addMarker({
+        lat: lat2,
+        lng: lng2,
+        title: "ROLE MODEL",
+        icon: "/img/logoo8.png",
+        infoWindow: {
+            content: "<h2>タイトル</h2><p><br/>説明文が入ります。</p>"
+        }
+    });
 
-        map.setStyle("map_style");
-      };
 
+    var styles = [{
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+            "color": "#444444"
+        }]
+    }, {
+        "featureType": "administrative.country",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "administrative.locality",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "administrative.neighborhood",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [{
+            "color": "#f2f2f2"
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "saturation": "17"
+        }]
+    }, {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "lightness": 45
+        }, {
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [{
+            "color": "#66c9f0"
+        }, {
+            "visibility": "on"
+        }]
+    }];
+
+    map.addStyle({
+        styledMapName: "Styled Map",
+        styles: styles,
+        mapTypeId: "map_style"
+    })
+
+    map.setStyle("map_style");
+
+};
